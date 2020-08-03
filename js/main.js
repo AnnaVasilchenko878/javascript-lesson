343a6e3c0e92be752c5c2de5ce4fb4ec034f7c2e
anna_vasilchenko <motolive2009@ramvler.ru>
2020-08-03 19:32:45 +0300

    Добавляет вопросы пользователю и расчитывает дневной бюджет

--- js/main.js
+++ js/main.js
@@ -1,47 +1,18 @@
-var first = 1;
-var leftBorderWidth = 2;
+var answerBudjet = +prompt('Ваш бюджет?');
+var marketName = prompt('Название вашего магазина?');
 
-const PI = 3.14;
+var mainList = {
+    budjet: answerBudjet,
+    market: marketName,
+    shopGoods: [],
+    employers: {},
+    open: true
+};
 
-{
-    let second = 2;
+for (var i = 0; i < 3; i++) {
+    var answerShopGoods = prompt('Какие товары вы собираетесь продавать?');
+    mainList.shopGoods.push(answerShopGoods);
 }
 
-var number = 2;
-var string = 'Hello';
-// используется редко
-var sym = Symbol()
-var boolean = true;
-null;
-undefined;
-var obj = {};
-
-// ininity
-// console.log(4 / 0);
-// NaN
-// console.log('hi' / 2);
-// undefined 
-var und;
-// console.log(und);
-// object
-var object = {
-    name: 'John',
-    age: 25,
-    isMarried: false
-};
-// console.log(object.name);
-var arr = ['apple', 'banana', 'orange'];
-// console.log(arr[2]);
-// confirm('Are you here?');
-// var answer = +prompt('Are you 18?', 18);
-// console.log(typeof (answer));
-var incr = 10;
-decr = 10;
-// console.log(++incr);
-// console.log(--decr);
-// console.log(5 % 2);
-// console.log('10' === 10);
-var isCheked = true;
-isClose = true;
-// console.log(isCheked && !isClose);
-console.log(isCheked || !isClose);
+var budjetOfDay = answerBudjet / 30;
+alert('Ваш бюджет на день составляет ' + budjetOfDay);
