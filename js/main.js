cb5a32995eb51ff43e1aecbd7b18f21c5f1622e6
anna_vasilchenko <motolive2009@ramvler.ru>
2020-08-03 18:40:53 +0300

    Первый коммитЭ

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var first = 1;
+var leftBorderWidth = 2;
+
+const PI = 3.14;
+
+{
+    let second = 2;
+}
+
+var number = 2;
+var string = 'Hello';
+// используется редко
+var sym = Symbol()
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+// ininity
+// console.log(4 / 0);
+// NaN
+// console.log('hi' / 2);
+// undefined 
+var und;
+// console.log(und);
+// object
+var object = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+// console.log(object.name);
+var arr = ['apple', 'banana', 'orange'];
+// console.log(arr[2]);
+// confirm('Are you here?');
+// var answer = +prompt('Are you 18?', 18);
+// console.log(typeof (answer));
+var incr = 10;
+decr = 10;
+// console.log(++incr);
+// console.log(--decr);
+// console.log(5 % 2);
+// console.log('10' === 10);
+var isCheked = true;
+isClose = true;
+// console.log(isCheked && !isClose);
+console.log(isCheked || !isClose);
