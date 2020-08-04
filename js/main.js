e8c8888e3e572443b9d7d54f5aa4868d7fbb16a8
anna_vasilchenko <motolive2009@ramvler.ru>
2020-08-04 15:04:31 +0300

    Добавляет проверку ввода данных и время работы

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
-var answerBudjet = +prompt('Ваш бюджет?');
-var marketName = prompt('Название вашего магазина?');
+let answerBudjet = +prompt('Ваш бюджет?'),
+    marketName = prompt('Название вашего магазина?'),
+    budjetOfDay = answerBudjet / 30,
+    time = 19,
 
-var mainList = {
-    budjet: answerBudjet,
-    market: marketName,
-    shopGoods: [],
-    employers: {},
-    open: true
-};
+    mainList = {
+        budjet: answerBudjet,
+        market: marketName,
+        shopGoods: [],
+        employers: {},
+        open: false
+    };
 
-for (var i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
     var answerShopGoods = prompt('Какие товары вы собираетесь продавать?');
-    mainList.shopGoods.push(answerShopGoods);
+    if ((typeof (answerShopGoods)) === 'string' && (typeof (answerShopGoods)) !== null && (typeof (answerShopGoods)) != '' && answerShopGoods.length < 50) {
+        mainList.shopGoods[i] = answerShopGoods;
+    }
+    // else {
+    // сделать так чтобы цикл запускался заново
+    // }
 }
 
-var budjetOfDay = answerBudjet / 30;
+if (time < 0) {
+    console.log('Такого не может быть');
+} else if (time > 8 && time < 20) {
+    console.log('Время работать');
+} else if (time < 24) {
+    console.log('Уже слишком поздно');
+} else if (time > 24) {
+    console.log('В сутках только 24 часа');
+}
+
 alert('Ваш бюджет на день составляет ' + budjetOfDay);
+console.log(mainList.shopGoods);
