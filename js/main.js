5af4874439a945a0b18168116c2b45820d309e96
anna_vasilchenko <motolive2009@ramvler.ru>
2020-08-05 14:44:22 +0300

    Добавляет функцию найма сотрудников

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function start() {
     marketName = prompt('Название вашего магазина?').toUpperCase();
     time = 19;
 }
-// start();
+start();
 
 
 let mainList = {
@@ -21,17 +21,14 @@ let mainList = {
     shopGoods: [],
     employers: {},
     open: false,
-    discount: false
+    discount: true
 };
 
 function discountCalc() {
-    if (mainList.discount) {
-        price = 0.8;
-        return price;
-    }
+    (mainList.discount) ? price = 0.8: price = 1;
+    return price;
 }
-
-console.log(discountCalc());
+discountCalc();
 
 function chooseGoods() {
     for (let i = 0; i < 5; i++) {
@@ -39,7 +36,15 @@ function chooseGoods() {
         ((typeof (answerShopGoods)) === 'string' && (typeof (answerShopGoods)) != null && answerShopGoods != '' && answerShopGoods.length < 50) ? mainList.shopGoods[i] = answerShopGoods: i = i - 1;
     }
 };
-// chooseGoods();
+chooseGoods();
+
+function hiringEmployers() {
+    for (let i = 0; i < 4; i++) {
+        let answerNameEmployer = prompt('Введите имя сотрудника');
+        ((typeof (answerNameEmployer)) === 'string' && (typeof (answerNameEmployer)) != null && answerNameEmployer != '' && answerNameEmployer.length < 50) ? mainList.employers[i] = answerNameEmployer: i = i - 1;
+    }
+};
+hiringEmployers();
 
 function workTime(time) {
     if (time < 0) {
@@ -52,12 +57,12 @@ function workTime(time) {
         console.log('В сутках только 24 часа');
     }
 };
-// workTime(15);
+workTime(15);
 
 function dailyBudget() {
     budjetOfDay = Math.round(answerBudjet / 30);
     alert('Ваш бюджет на день составляет ' + budjetOfDay);
 };
-// dailyBudget();
+dailyBudget();
 
-// console.log(mainList);
+console.log(mainList);
