1b358b0db2cfb65a094a9acc65a3bb63b88a6088
anna_vasilchenko <motolive2009@ramvler.ru>
2020-08-06 17:55:27 +0300

    Добавляет методы в главный объект

--- js/main.js
+++ js/main.js
@@ -21,48 +21,39 @@ let mainList = {
     shopGoods: [],
     employers: {},
     open: false,
-    discount: true
-};
-
-function discountCalc() {
-    (mainList.discount) ? price = 0.8: price = 1;
-    return price;
-}
-discountCalc();
-
-function chooseGoods() {
-    for (let i = 0; i < 5; i++) {
-        let answerShopGoods = prompt('Какие товары вы собираетесь продавать?', '');
-        ((typeof (answerShopGoods)) === 'string' && (typeof (answerShopGoods)) != null && answerShopGoods != '' && answerShopGoods.length < 50) ? mainList.shopGoods[i] = answerShopGoods: i = i - 1;
+    discount: true,
+    iscountCalc: function discountCalc() {
+        (mainList.discount) ? price = 0.8: price = 1;
+        return price;
+    },
+    chooseGoods: function chooseGoods() {
+        for (let i = 0; i < 5; i++) {
+            let answerShopGoods = prompt('Какие товары вы собираетесь продавать?', '');
+            ((typeof (answerShopGoods)) === 'string' && (typeof (answerShopGoods)) != null && answerShopGoods != '' && answerShopGoods.length < 50) ? mainList.shopGoods[i] = answerShopGoods: i = i - 1;
+        }
+    },
+    hiringEmployers: function hiringEmployers() {
+        for (let i = 0; i < 4; i++) {
+            let answerNameEmployer = prompt('Введите имя сотрудника', '');
+            ((typeof (answerNameEmployer)) === 'string' && (typeof (answerNameEmployer)) != null && answerNameEmployer != '' && answerNameEmployer.length < 50) ? mainList.employers[i] = answerNameEmployer: i = i - 1;
+        }
+    },
+    workTime: function workTime(time) {
+        if (time < 0) {
+            console.log('Такого не может быть');
+        } else if (time > 8 && time < 20) {
+            console.log('Время работать');
+            mainList.open = true;
+        } else if (time < 24) {
+            console.log('Уже слишком поздно');
+        } else if (time > 24) {
+            console.log('В сутках только 24 часа');
+        }
+    },
+    dailyBudget: function dailyBudget() {
+        budjetOfDay = Math.round(answerBudjet / 30);
+        alert('Ваш бюджет на день составляет ' + budjetOfDay);
     }
 };
-chooseGoods();
-
-function hiringEmployers() {
-    for (let i = 0; i < 4; i++) {
-        let answerNameEmployer = prompt('Введите имя сотрудника', '');
-        ((typeof (answerNameEmployer)) === 'string' && (typeof (answerNameEmployer)) != null && answerNameEmployer != '' && answerNameEmployer.length < 50) ? mainList.employers[i] = answerNameEmployer: i = i - 1;
-    }
-};
-hiringEmployers();
-
-function workTime(time) {
-    if (time < 0) {
-        console.log('Такого не может быть');
-    } else if (time > 8 && time < 20) {
-        console.log('Время работать');
-    } else if (time < 24) {
-        console.log('Уже слишком поздно');
-    } else if (time > 24) {
-        console.log('В сутках только 24 часа');
-    }
-};
-workTime(15);
-
-function dailyBudget() {
-    budjetOfDay = Math.round(answerBudjet / 30);
-    alert('Ваш бюджет на день составляет ' + budjetOfDay);
-};
-dailyBudget();
 
 console.log(mainList);
