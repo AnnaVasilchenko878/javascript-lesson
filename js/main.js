240e2b7bfd5c9fbd76e740f31f72a26b68985465
anna_vasilchenko <motolive2009@ramvler.ru>
2020-08-13 11:41:43 +0300

    Добавляет оставшейся фукционал

--- js/main.js
+++ js/main.js
@@ -1,67 +1,114 @@
-let answerBudjet,
-    budjetOfDay,
-    marketName,
-    time,
-    price;
+ let btnOpen = document.querySelector('#open-btn'),
+     budgetShop = document.querySelector('.budget-value'),
+     nameShop = document.querySelector('.name-value'),
+     goodsCategoryProducts = document.querySelector('.goods-item-btn'),
+     inputGoodItems = document.querySelectorAll('.goods-item'),
+     goodsValue = document.querySelector('.goods-value'),
+     chooseItem = document.querySelector('.choose-item'),
+     itemsValue = document.querySelector('.items-value'),
+     timeValue = document.querySelector('.time-value'),
+     openValue = document.querySelector('.isopen-value'),
+     countBudget = document.querySelector('.count-budget-btn'),
+     countBudgetValue = document.querySelector('.count-budget-value'),
+     hireEmployersItem = document.querySelectorAll('.hire-employers-item'),
+     hireEmployersBtn = document.querySelector('.hire-employers-btn'),
+     employersValue = document.querySelector('.employers-value'),
+     discountValue = document.querySelector('.discount-value');
 
-function start() {
-    answerBudjet = prompt('Ваш бюджет?', '');
-    while (isNaN(answerBudjet) || answerBudjet == '' || answerBudjet == null) {
-        answerBudjet = prompt('Ваш бюджет?', '');
-    }
-    marketName = prompt('Название вашего магазина?', '').toUpperCase();
-    time = 19;
-}
-// start();
 
 
-let mainList = {
-    budjet: answerBudjet,
-    market: marketName,
-    shopGoods: [],
-    employers: {},
-    open: false,
-    discount: true,
-    shopItems: [],
-    iscountCalc: function discountCalc() {
-        (mainList.discount) ? price = 0.8: price = 1;
-        return price;
-    },
-    chooseGoods: function chooseGoods() {
-        for (let i = 0; i < 5; i++) {
-            let answerShopGoods = prompt('Какие товары вы собираетесь продавать?', '');
-            ((typeof (answerShopGoods)) === 'string' && (typeof (answerShopGoods)) != null && answerShopGoods != '' && answerShopGoods.length < 50) ? mainList.shopGoods[i] = answerShopGoods: i = i - 1;
-        }
-    },
-    hiringEmployers: function hiringEmployers() {
-        for (let i = 0; i < 4; i++) {
-            let answerNameEmployer = prompt('Введите имя сотрудника', '');
-            ((typeof (answerNameEmployer)) === 'string' && (typeof (answerNameEmployer)) != null && answerNameEmployer != '' && answerNameEmployer.length < 50) ? mainList.employers[i] = answerNameEmployer: i = i - 1;
-        }
-    },
-    workTime: function workTime(time) {
-        if (time < 0) {
-            console.log('Такого не может быть');
-        } else if (time > 8 && time < 20) {
-            console.log('Время работать');
-            mainList.open = true;
-        } else if (time < 24) {
-            console.log('Уже слишком поздно');
-        } else if (time > 24) {
-            console.log('В сутках только 24 часа');
-        }
-    },
-    dailyBudget: function dailyBudget() {
-        budjetOfDay = Math.round(answerBudjet / 30);
-        alert('Ваш бюджет на день составляет ' + budjetOfDay);
-    },
-    chooseShopItem: function chooseShopItem() {
-        let items = prompt('Перечислите через запятую ваши товары', '');
-        ((typeof (items)) === 'string' && items) ? mainList.shopItems = items.split(','): items = prompt('Перечислите через запятую ваши товары', '');
-        mainList.shopItems.push(prompt('Все товары вы учли?', ''));
-        mainList.shopItems.sort();
-    }
-};
-console.log(mainList.chooseShopItem());
-// console.log(mainList.shopItems);
-// console.log(mainList);
+ let answerBudget,
+     budgetOfDay,
+     time,
+     price;
+
+ btnOpen.addEventListener('click', () => {
+     answerBudget = prompt('Ваш бюджет?', '');
+     while (isNaN(answerBudget) || answerBudget == '' || answerBudget == null) {
+         answerBudget = prompt('Ваш бюджет?', '');
+     }
+     budgetShop.textContent = answerBudget;
+     nameShop.textContent = prompt('Название вашего магазина?', '').toUpperCase();
+
+     let discountAnswer = confirm('Есть ли вашем магазине дисконтная система?');
+     if (discountAnswer == true) {
+         price = (price * 100) / 80;
+         mainList.discount = true;
+         discountValue.style.backgroundColor = '#2e856e';
+     } else {
+         mainList.discount = false;
+         discountValue.style.backgroundColor = '#e3242b';
+     }
+ });
+
+ goodsCategoryProducts.addEventListener('click', () => {
+     for (let i = 0; i < inputGoodItems.length; i++) {
+         let answerShopGoods = inputGoodItems[i].value;
+         if ((typeof (answerShopGoods)) === 'string' && answerShopGoods.length < 50) {
+             mainList.shopGoods[i] = answerShopGoods;
+             goodsValue.textContent = mainList.shopGoods;
+         } else {
+             i = i - 1;
+         }
+     }
+ });
+
+ chooseItem.addEventListener('change', () => {
+     let items = chooseItem.value;
+     if (isNaN(items) && items != '') {
+         mainList.shopItems = items.split(', ');
+         mainList.shopItems.sort();
+         itemsValue.textContent = mainList.shopItems;
+     }
+ });
+
+ timeValue.addEventListener('change', () => {
+     let time = timeValue.value;
+     if (time < 0) {
+         console.log('Такого не может быть');
+         mainList.open = false;
+     } else if (time > 8 && time < 20) {
+         console.log('Время работать');
+         mainList.open = true;
+     } else if (time < 24) {
+         console.log('Уже слишком поздно');
+         mainList.open = false;
+     } else if (time > 24) {
+         console.log('В сутках только 24 часа');
+         mainList.open = false;
+     }
+
+     if (mainList.open == true) {
+         openValue.style.backgroundColor = '#2e856e';
+     } else {
+         openValue.style.backgroundColor = '#e3242b';
+
+     }
+ });
+
+ countBudget.addEventListener('click', () => {
+     countBudgetValue.value = Math.round(answerBudget / 30);
+ });
+ hireEmployersBtn.addEventListener('click', () => {
+
+     for (let i = 0; i < hireEmployersItem.length; i++) {
+         let answerNameEmployer = hireEmployersItem[i].value;
+         if ((typeof (answerNameEmployer)) === 'string' && (typeof (answerNameEmployer)) != null && answerNameEmployer.length < 50) {
+             mainList.employers[i] = answerNameEmployer;
+             employersValue.textContent += mainList.employers[i] + ', ';
+         } else {
+             i = i - 1;
+         }
+     }
+ });
+
+
+ let mainList = {
+     budget: answerBudget,
+     market: nameShop,
+     shopGoods: [],
+     employers: {},
+     open: false,
+     discount: true,
+     shopItems: []
+ };
